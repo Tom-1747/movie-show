3ac371e578976eda259eacff346d2c0f7ba23817
yoandiny <63061998+yoandiny@users.noreply.github.com>
2026-08-05 02:27:26 +0300

    fix(projections): disable security filters in controller tests (#12)

--- src/test/java/com/movie/demo/endpoint/rest/controller/ProjectionControllerTest.java
+++ src/test/java/com/movie/demo/endpoint/rest/controller/ProjectionControllerTest.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -32,6 +33,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.server.ResponseStatusException;
 
 @WebMvcTest(controllers = ProjectionController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @Import(RestExceptionHandler.class)
 class ProjectionControllerTest {
 
